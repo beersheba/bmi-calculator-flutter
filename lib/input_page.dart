e091846a3491a4313412d9d9d7dfd834830583d4
Andrey Rankov <andreyr@appcard.com>
2020-10-17 15:56:16 +0300

    Bottom container margin

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -59,6 +59,7 @@ class _InputPageState extends State<InputPage> {
           ),
           Container(
             color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: bottomContainerHeight,
           )
